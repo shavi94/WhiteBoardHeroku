c6a98f69a27b10ec47b0a4cc59e1d83a827d68d4
shavi94 <30893096+shavi94@users.noreply.github.com>
2019-06-24 00:33:55 +0530

    poll added

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 var socket = require('socket.io');
 var usernames = [];
-
+var voting = [];
+var labels = [];
 
 app.use('/static', express.static('pub'));
 
@@ -27,6 +28,64 @@ function newConnection(socket){
     socket.on('sendmessage',sendmessage);
     socket.on('new user',newUser);
     socket.on('file', sharefile);
+    socket.on('create poll',createPoll);
+    socket.on('vote categorize',voteCategorize);
+    socket.on('vote result',voteResults);
+
+    function voteResults(id){
+        var lab1;
+        var lab2;
+        var lab3;
+
+        var countlab1=0;
+        var countlab2=0;
+        var countlab3=0;
+
+        for(var y=0;y<labels.length;y++){
+            if(labels[y][0]==id){
+                lab1 = labels[y][1].label1;
+                lab2 = labels[y][1].label2;
+                lab3 = labels[y][1].label3;
+            }
+        }
+
+        for(var x=0; x<voting.length;x++){
+            if(voting[x][0]==id){
+                if(voting[x][1].vote==lab1){
+                    countlab1++;
+                }else if(voting[x][1].vote==lab2){
+                    countlab2++;
+                }else if(voting[x][1].vote==lab3){
+                    countlab3++;
+                }
+            }
+        }
+
+        console.log(id+" counts: "+lab1+" : "+countlab1+"  "+
+        lab2+" : "+countlab2+"  "+lab3+" : "+countlab3);
+    }
+
+    function voteCategorize(vote,nid){
+        var arrdata = [nid, {
+            vote: vote,
+            user: socket.username
+        }];
+
+        voting.push(arrdata);
+        console.log(arrdata);
+    }
+
+
+    function createPoll(quiz,opt1,opt2,opt3,id){
+        var name = socket.username;
+        var dataArr = [id, {
+            label1 : opt1,
+            label2 : opt2,
+            label3 : opt3
+        }];
+        labels.push(dataArr);
+        socket.broadcast.emit('pollonchat',quiz,opt1,opt2,opt3,name,id);
+    }
 
     //share files
     function sharefile(dataURI,type){
